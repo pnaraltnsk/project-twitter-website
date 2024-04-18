dc486e39b62ca5b85bf2cb6e872179c23c160ae9
pinaraltinisik <pinar.altinisik@student.griffith.ie>
2024-04-18 22:01:38 +0100

    Git commit history

---
+++ static/app-setup.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCL0htPzZa2S5L9c0WMqD34ZIFlCAN-4ao",
+  authDomain: "assignment3-348023.firebaseapp.com",
+  projectId: "assignment3-348023",
+  storageBucket: "assignment3-348023.appspot.com",
+  messagingSenderId: "77662886191",
+  appId: "1:77662886191:web:a1424641eaf49b335029da"
+};
+
+firebase.initializeApp(firebaseConfig);
